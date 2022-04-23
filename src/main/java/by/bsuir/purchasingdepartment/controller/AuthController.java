20ceb9e3242fce3891c8cd2bb4fef5738224cec2
masha <masha.k03@mail.ru>
2022-04-23 20:21:51 +0300

    back+front working auth

--- src/main/java/by/bsuir/purchasingdepartment/controller/AuthController.java
+++ src/main/java/by/bsuir/purchasingdepartment/controller/AuthController.java
@@ -1,21 +1,48 @@
 package by.bsuir.purchasingdepartment.controller;
 
+import by.bsuir.purchasingdepartment.security.dto.JwtResp;
 import by.bsuir.purchasingdepartment.service.UserService;
+import by.bsuir.purchasingdepartment.service.dto.SignInDto;
+import by.bsuir.purchasingdepartment.service.dto.SignUpDto;
+import by.bsuir.purchasingdepartment.service.dto.UserDto;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+
+@Controller
 @RequestMapping(value = "/auth")
 @AllArgsConstructor
 public class AuthController {
     private final UserService userService;
-
+    @GetMapping
+    public String auth(Model model) {
+        model.addAttribute("user",  new SignInDto());
+        return "auth";
+    }
+//
+//    @RequestMapping(method = RequestMethod.POST)
+//    public String authPost(@ModelAttribute("user") User user) {
+//        //проверка наличия
+//
+//        return "redirect:/orders";
+//    }
     @PostMapping("/sign-in")
     //TODO доделать проверку valid
-    public ResponseEntity<JwtResp> signIn(@RequestBody SignInDto request) {
-        JwtResp resp = userService.signIn(request);
-        return new ResponseEntity<>(resp, HttpStatus.OK);
+    public String signIn(@ModelAttribute("user") SignInDto request) {
+        JwtResp resp = null;
+        try {
+            resp = userService.signIn(request);
+        } catch (AuthenticationException e) {
+            resp=new JwtResp(null, null);
+            return "auth";
+        }
+        //return new ResponseEntity<>(resp, HttpStatus.OK);
+        return "orders";
     }
 
     @PostMapping("/sign-up")
