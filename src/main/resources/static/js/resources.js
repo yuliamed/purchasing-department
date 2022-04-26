f98e7e188ff19b44e6dd438f7c35f5aac77c7faa
masha <masha.k03@mail.ru>
2022-04-26 12:34:40 +0300

    валидация при добавлении ресурса (поля без id)

--- src/main/resources/static/js/resources.js
+++ src/main/resources/static/js/resources.js
@@ -1,92 +1,98 @@
 try {
-  document && document.addEventListener('DOMContentLoaded', onLoad);
+    document && document.addEventListener('DOMContentLoaded', onLoad);
 } catch (e) {
-  console.warn(e);
+    console.warn(e);
 }
 
-const onAddRow = () => {};
+const onAddRow = () => {
+};
 
 function onLoad() {
-  const buttonAddResource = document.querySelector('[data-related=add-resource]');
-  const modal = document.querySelector('[data-modal]');
+    const buttonAddResource = document.querySelector('[data-related=add-resource]');
+    const modal = document.querySelector('[data-modal]');
 
-  if (modal) {
-    modal && configureModal(modal);
-    modal && configureModalForm(modal);
-  }
+    if (modal) {
+        modal && configureModal(modal);
+        modal && configureModalForm(modal);
+    }
 
-  buttonAddResource && buttonAddResource.addEventListener('click', e => {
-    if (modal && modal.classList.contains('hide')) toggleModal(modal, true);
-  });
+    buttonAddResource && buttonAddResource.addEventListener('click', e => {
+        if (modal && modal.classList.contains('hide')) toggleModal(modal, true);
+    });
 }
 
 function toggleModal(modal, toggler) {
-  if (toggler) modal.classList.remove('hide');
-  else modal.classList.add('hide');
+    if (toggler) modal.classList.remove('hide');
+    else modal.classList.add('hide');
 }
 
 function configureModal(modal) {
-  const closeBtn = modal.querySelector('.modal-close');
-  const modalWrapper = modal.querySelector('.modal-wrapper');
-  closeBtn && closeBtn.addEventListener('click', e => {
-    toggleModal(modal, false);
-  });
-  modal && modal.addEventListener('click', e => {
-    e.stopPropagation();
-    if (e.target.classList.contains('modal-wrapper')) toggleModal(modal, false);
-  });
+    const closeBtn = modal.querySelector('.modal-close');
+    const modalWrapper = modal.querySelector('.modal-wrapper');
+    closeBtn && closeBtn.addEventListener('click', e => {
+        toggleModal(modal, false);
+    });
+    modal && modal.addEventListener('click', e => {
+        e.stopPropagation();
+        if (e.target.classList.contains('modal-wrapper')) toggleModal(modal, false);
+    });
 }
 
 function configureModalForm(modal) {
-  const inputId = modal.querySelector('#in-id');
-  const inputName = modal.querySelector('#in-name');
-  const inputDescription = modal.querySelector('#in-description');
-  const form = modal.querySelector('form.modal-form');
-  //TODO вернуть валидацию как и при авторизации
-  //
-  // form && form.addEventListener('submit', e => {
-  //   e.preventDefault();
-  //   if (!inputId || !inputName || !inputDescription) return;
-  //
-  //   if (
-  //     !inputId.value.trim() ||
-  //     !inputName.value.trim() ||
-  //     !inputDescription.value.trim()
-  //   ) return alert('Заполните поля');
-  //
-  //   const data = {
-  //     checkMark: false,
-  //     id: inputId.value.trim(),
-  //     name: inputName.value.trim(),
-  //     description: inputDescription.value.trim()
-  //   };
-  //
-  //   addResourceToTable(data);
-  //   onAddRow();
-  //   toggleModal(modal, false);
-  //
-  //   inputId.value = '';
-  //   inputName.value = '';
-  //   inputDescription.value = '';
-  // });
+    //const inputId = modal.querySelector('#in-id');
+    const inputName = modal.querySelector('#in-name');
+    const inputDescription = modal.querySelector('#in-description');
+    const form = modal.querySelector('form.modal-form');
+
+    form && form.addEventListener('submit', e => {
+
+        //if (!inputId || !inputName || !inputDescription){
+            if (!inputName || !inputDescription){
+            e.preventDefault();
+            return;}
+
+        if (
+            //!inputId.value.trim() ||
+            !inputName.value.trim() ||
+            !inputDescription.value.trim()
+        ) {
+            e.preventDefault();
+            return alert('Заполните поля');
+        }
+
+        // const data = {
+        //     checkMark: false,
+        //     id: inputId.value.trim(),
+        //     name: inputName.value.trim(),
+        //     description: inputDescription.value.trim()
+        // };
+
+        // addResourceToTable(data);
+        // onAddRow();
+        // toggleModal(modal, false);
+        //
+        // inputId.value = '';
+        // inputName.value = '';
+        // inputDescription.value = '';
+    });
 }
 
 function addResourceToTable(data) {
-  if (!data) return;
-  const table = document.querySelector('#resources_table');
-  const tbody = table.querySelector('tbody');
+    if (!data) return;
+    const table = document.querySelector('#resources_table');
+    const tbody = table.querySelector('tbody');
 
-  const tr = document.createElement('tr');
+    const tr = document.createElement('tr');
 
-  for (const dataKey in data) {
-    const td = document.createElement('td');
-    if (dataKey === 'checkMark') {
-      td.className = data[dataKey] ? 'check-mark-active' : '';
-      td.innerText = '';
-    } else {
-      td.innerText = data[dataKey];
+    for (const dataKey in data) {
+        const td = document.createElement('td');
+        if (dataKey === 'checkMark') {
+            td.className = data[dataKey] ? 'check-mark-active' : '';
+            td.innerText = '';
+        } else {
+            td.innerText = data[dataKey];
+        }
+        tr.append(td);
     }
-    tr.append(td);
-  }
-  tbody.append(tr);
+    tbody.append(tr);
 }
