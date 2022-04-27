5adbafffdda7a0e3cc1e590a3dd49b1fa937d092
masha <masha.k03@mail.ru>
2022-04-28 00:16:20 +0300

    отображение страницы создания продукта (выскакивает ошибка в логе)

--- src/main/java/by/bsuir/purchasingdepartment/controller/ProductController.java
+++ src/main/java/by/bsuir/purchasingdepartment/controller/ProductController.java
@@ -1,9 +1,11 @@
 package by.bsuir.purchasingdepartment.controller;
 
 import by.bsuir.purchasingdepartment.entity.Product;
+import by.bsuir.purchasingdepartment.entity.Resource;
 import by.bsuir.purchasingdepartment.repository.ResourceRepository;
 import by.bsuir.purchasingdepartment.service.ProductService;
 import by.bsuir.purchasingdepartment.service.dto.DeleteDto;
+import by.bsuir.purchasingdepartment.service.dto.ProductDto;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -51,5 +53,12 @@ public class ProductController {
             productService.deleteProduct(delIds.getDelIds().get(i));
         }
         return returnText;
+    }
+    @GetMapping(value = "/create-product")
+    public String showProductAddPage(Model model) {
+        List<Resource> resources = resourceRepository.findAll();
+        model.addAttribute("new_product", new ProductDto());
+        model.addAttribute("resources", resources);
+        return "create-product";
     }
 }
