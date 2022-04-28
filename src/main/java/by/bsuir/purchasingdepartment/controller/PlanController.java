6f4903519fb9a7ad3162186b504f6160af70a08c
yuliamed <yuliamed@list.ru>
2022-04-28 12:29:14 +0300

    add plan service + front file processing

--- src/main/java/by/bsuir/purchasingdepartment/controller/PlanController.java
+++ src/main/java/by/bsuir/purchasingdepartment/controller/PlanController.java
@@ -2,19 +2,27 @@ package by.bsuir.purchasingdepartment.controller;
 
 import by.bsuir.purchasingdepartment.service.PlanService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping(value = "/plans")
 @AllArgsConstructor
 public class PlanController {
+    private final PlanService planService;
+
     @GetMapping
     public String showPlanPage(Model model) {
         return "plans";
     }
- //   private final PlanService planService;
+
+    @PostMapping("/add")
+    public String addFileWithPlan(@RequestParam("file")MultipartFile file){
+        //String s = file.getContentType();
+        planService.addPlansFromFile(file);
+        return "redirect:";
+    }
 }
--- src/main/java/by/bsuir/purchasingdepartment/service/PlanService.java
+++ src/main/java/by/bsuir/purchasingdepartment/service/PlanService.java
@@ -1,7 +1,11 @@
 package by.bsuir.purchasingdepartment.service;
 
-import by.bsuir.purchasingdepartment.repository.PlanRepository;
+import by.bsuir.purchasingdepartment.entity.Plan;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface PlanService {
-
+    List<Plan> findAll();
+    void addPlansFromFile(MultipartFile file);
 }
