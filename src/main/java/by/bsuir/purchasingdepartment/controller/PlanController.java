9ca08e955e368c655e524ac511508342ddd013b1
yuliamed <yuliamed@list.ru>
2022-05-08 18:25:01 +0300

    jwt for auth

--- src/main/java/by/bsuir/purchasingdepartment/controller/PlanController.java
+++ src/main/java/by/bsuir/purchasingdepartment/controller/PlanController.java
@@ -3,10 +3,13 @@ package by.bsuir.purchasingdepartment.controller;
 import by.bsuir.purchasingdepartment.entity.Plan;
 import by.bsuir.purchasingdepartment.service.PlanService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/plans")
 @AllArgsConstructor
+@PreAuthorize("hasAnyAuthority('USER')")
 public class PlanController {
     private final PlanService planService;
 
@@ -25,7 +29,7 @@ public class PlanController {
     }
 
     @PostMapping("/add")
-    public String addFileWithPlan(@RequestParam("file")MultipartFile file){
+    public String addFileWithPlan(@RequestParam("file") MultipartFile file) {
         planService.addPlansFromFile(file);
         return "redirect:";
     }
