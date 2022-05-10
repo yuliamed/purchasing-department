947b4be150eda29a9ad0d98e418a0b4b9b0dda92
masha <masha.k03@mail.ru>
2022-05-10 22:35:36 +0300

    сортировки

--- src/main/resources/static/js/providers.js
+++ src/main/resources/static/js/providers.js
@@ -15,10 +15,26 @@
     };
 
     function onLoad() {
+        const getSort = ({ target }) => {
+            const order = (target.dataset.order = -(target.dataset.order || -1));
+            const index = [...target.parentNode.cells].indexOf(target);
+            const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
+            const comparator = (index, order) => (a, b) => order * collator.compare(
+                b.children[index].innerHTML,
+                a.children[index].innerHTML
+            );
+
+            for(const tBody of target.closest('table').tBodies)
+                tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+
+            for(const cell of target.parentNode.cells)
+                cell.classList.toggle('sorted', cell === target);
+        };
+        document.querySelectorAll('.content-table thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
 
 
 
-       // document.getElementById("id123").innerHTML = 3;
+        // document.getElementById("id123").innerHTML = 3;
         const table = getTablesWithActions();
 
         // const mutationObserver = new MutationObserver(() => {
